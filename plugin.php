6d171b5d4d3860fa34cebab56d47147268615707
Brandon <brandon@tendency.me>
2024-04-01 12:21:22 -0500

    🚨 Run Pint

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  * Plugin Name: Nofollow Pretty Links
  * Plugin URI:  https://github.com/log1x/nofollow-pretty-links
  * Description: Add `rel="nofollow"` and `target="_blank"` to URL's created by Pretty Links
- * Version:     1.0.2
+ * Version:     1.0.3
  * Author:      Brandon Nifong
  * Author URI:  https://github.com/log1x
  * Licence:     MIT
@@ -21,9 +21,9 @@ add_action('init', new class
      */
     public function __invoke()
     {
-        require_once file_exists($composer = __DIR__ . '/vendor/autoload.php') ?
+        require_once file_exists($composer = __DIR__.'/vendor/autoload.php') ?
             $composer :
-            __DIR__ . '/dist/autoload.php';
+            __DIR__.'/dist/autoload.php';
 
         return new NofollowPrettyLinks(
             plugin_dir_path(__FILE__),
