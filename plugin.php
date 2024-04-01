f9eb07b18fc1ba858ef55ae9c17dcd9a842526d9
Brandon <brandon@tendency.me>
2024-04-01 12:32:06 -0500

    🩹 Fix plugin autoloader

--- plugin.php
+++ plugin.php
@@ -4,13 +4,17 @@
  * Plugin Name: Nofollow Pretty Links
  * Plugin URI:  https://github.com/log1x/nofollow-pretty-links
  * Description: Add `rel="nofollow"` and `target="_blank"` to URL's created by Pretty Links
- * Version:     1.0.3
+ * Version:     1.0.4
  * Author:      Brandon Nifong
  * Author URI:  https://github.com/log1x
  * Licence:     MIT
  */
 
 namespace Log1x\Plugin\NofollowPrettyLinks;
+
+if (file_exists($composer = __DIR__.'/vendor/autoload.php')) {
+    require_once $composer;
+}
 
 add_action('init', new class
 {
@@ -21,10 +25,6 @@ add_action('init', new class
      */
     public function __invoke()
     {
-        require_once file_exists($composer = __DIR__.'/vendor/autoload.php') ?
-            $composer :
-            __DIR__.'/dist/autoload.php';
-
         return new NofollowPrettyLinks(
             plugin_dir_path(__FILE__),
             plugin_dir_url(__FILE__)
